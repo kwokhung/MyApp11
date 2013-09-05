ef43227f0b83422ce2f1c55b8f22dfcbd82f3e53
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-09-05 17:32:21 +0800

    test

--- js/main.js
+++ js/main.js
@@ -183,87 +183,88 @@ var main = function () {
         var whatToDo = function (data) {
             switch (data.what.toDo) {
                 case "updateYourDetails":
-                    edge.func("cs", function () {/*
-                        #r "System.Management.dll"
+                    edge.func("cs", {
+                        source: function () {/*
+                            using System;
+                            using System.Linq;
+                            using System.Management;
 
-                        using System;
-                        using System.Linq;
-                        using System.Management;
-
-                        async (dynamic input) =>
-                        {
-                            ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_LogicalDisk");
-
-                            ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
-
-                            Object[] disks = new Object[] { };
-
-                            foreach (ManagementObject item in searcher.Get())
+                            async (dynamic input) =>
                             {
-                                Console.WriteLine("Name = " + item["Name"]);
-                                Console.WriteLine("Size = {0:#,###.##} bytes", item["Size"]);
-                                Console.WriteLine("Size = {0:#,###.##} GB", (double)Convert.ToInt64(item["Size"]) / 1024 / 1024 / 1024);
-                                Console.WriteLine("FreeSpace = {0:#,###.##} bytes", item["FreeSpace"]);
-                                Console.WriteLine("FreeSpace = {0:#,###.##} GB", (double)Convert.ToInt64(item["FreeSpace"]) / 1024 / 1024 / 1024);
+                                ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_LogicalDisk");
 
-                                disks = disks.Concat(new[]
+                                ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
+
+                                Object[] disks = new Object[] { };
+
+                                foreach (ManagementObject item in searcher.Get())
                                 {
-                                    new
+                                    Console.WriteLine("Name = " + item["Name"]);
+                                    Console.WriteLine("Size = {0:#,###.##} bytes", item["Size"]);
+                                    Console.WriteLine("Size = {0:#,###.##} GB", (double)Convert.ToInt64(item["Size"]) / 1024 / 1024 / 1024);
+                                    Console.WriteLine("FreeSpace = {0:#,###.##} bytes", item["FreeSpace"]);
+                                    Console.WriteLine("FreeSpace = {0:#,###.##} GB", (double)Convert.ToInt64(item["FreeSpace"]) / 1024 / 1024 / 1024);
+
+                                    disks = disks.Concat(new[]
                                     {
-                                        name = item["Name"],
-                                        size = item["Size"],
-                                        freeSpace = item["FreeSpace"]
-                                    }
-                                }).ToArray();
-                            }           
+                                        new
+                                        {
+                                            name = item["Name"],
+                                            size = item["Size"],
+                                            freeSpace = item["FreeSpace"]
+                                        }
+                                    }).ToArray();
+                                }           
 
-                            winQuery = new ObjectQuery("Select * from Win32_Process");
+                                winQuery = new ObjectQuery("Select * from Win32_Process");
 
-                            searcher = new ManagementObjectSearcher(winQuery);
+                                searcher = new ManagementObjectSearcher(winQuery);
 
-                            Object[] processes = new Object[] { };
+                                Object[] processes = new Object[] { };
 
-                            foreach (ManagementObject item in searcher.Get())
-                            {
-                                Console.WriteLine("Name = " + item["Name"]);
-                                Console.WriteLine("ProcessId = " + item["ProcessId"]);
-
-                                String[] outputFields = new String[2];
-                                item.InvokeMethod("GetOwner", (Object[])outputFields);
-                                Console.WriteLine("User = " + outputFields[1] + "\\" + outputFields[0]);
-                                Console.WriteLine("CreationDate = " + item["CreationDate"]);
-                                Console.WriteLine("Priority = " + item["Priority"]);
-                                Console.WriteLine("WorkingSetSize = {0:#,###.##} KB", (double)Convert.ToInt64(item["WorkingSetSize"]) / 1024);
-
-                                processes = processes.Concat(new[]
+                                foreach (ManagementObject item in searcher.Get())
                                 {
-                                    new
+                                    Console.WriteLine("Name = " + item["Name"]);
+                                    Console.WriteLine("ProcessId = " + item["ProcessId"]);
+
+                                    String[] outputFields = new String[2];
+                                    item.InvokeMethod("GetOwner", (Object[])outputFields);
+                                    Console.WriteLine("User = " + outputFields[1] + "\\" + outputFields[0]);
+                                    Console.WriteLine("CreationDate = " + item["CreationDate"]);
+                                    Console.WriteLine("Priority = " + item["Priority"]);
+                                    Console.WriteLine("WorkingSetSize = {0:#,###.##} KB", (double)Convert.ToInt64(item["WorkingSetSize"]) / 1024);
+
+                                    processes = processes.Concat(new[]
                                     {
-                                        name = item["Name"],
-                                        processId = item["ProcessId"],
-                                        user = outputFields[1] + "\\" + outputFields[0],
-                                        creationDate = item["CreationDate"],
-                                        priority = item["Priority"],
-                                        workingSetSize = item["WorkingSetSize"],
-                                    }
-                                }).ToArray();
-                            }
-
-                            return new
-                            {
-                                data = new
-                                {
-                                    who = input.data.who,
-                                    OSVersion = Environment.OSVersion.ToString(),
-                                    MachineName = Environment.MachineName,
-                                    UserName = Environment.UserName,
-                                    UserDomainName = Environment.UserDomainName,
-                                    disks = disks,
-                                    processes = processes
+                                        new
+                                        {
+                                            name = item["Name"],
+                                            processId = item["ProcessId"],
+                                            user = outputFields[1] + "\\" + outputFields[0],
+                                            creationDate = item["CreationDate"],
+                                            priority = item["Priority"],
+                                            workingSetSize = item["WorkingSetSize"],
+                                        }
+                                    }).ToArray();
                                 }
-                            };
-                        }
-                    */})({
+
+                                return new
+                                {
+                                    data = new
+                                    {
+                                        who = input.data.who,
+                                        OSVersion = Environment.OSVersion.ToString(),
+                                        MachineName = Environment.MachineName,
+                                        UserName = Environment.UserName,
+                                        UserDomainName = Environment.UserDomainName,
+                                        disks = disks,
+                                        processes = processes
+                                    }
+                                };
+                            }
+                        */},
+                        references: ["System.Management.dll"]
+                    })({
                         data: {
                             who: who
                         }
