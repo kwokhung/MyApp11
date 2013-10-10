d90c80d1e33ca8bd7c83b5d13e5afc934b9fd060
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-10-10 09:45:28 +0800

    test

---
+++ js/app/util/ResourceInboundHelper.js
@@ -0,0 +1,64 @@
+﻿define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/_base/array"
+], function (declare, lang, array) {
+    return declare("app.util.ResourceInboundHelper", null, {
+        resourceMonitor: null,
+        constructor: function (kwArgs) {
+            lang.mixin(this, kwArgs);
+        },
+        onMessage: function () {
+            if (this.resourceMonitor.socket == null) {
+                return;
+            }
+
+            this.resourceMonitor.socket.on("heartbeat", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "heartbeat", what: data.when });
+                this.resourceMonitor.heartbeat();
+            }));
+
+            this.resourceMonitor.socket.on("you.are", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "you.are", what: data.who });
+            }));
+
+            this.resourceMonitor.socket.on("you.are.no.more", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "you.are.no.more", what: data.who });
+            }));
+
+            this.resourceMonitor.socket.on("he.is", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "he.is", what: data.who });
+            }));
+
+            this.resourceMonitor.socket.on("he.is.no.more", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "he.is.no.more", what: data.who });
+            }));
+
+            this.resourceMonitor.socket.on("there.are", lang.hitch(this, function (data) {
+                array.forEach(data.who, lang.hitch(this, function (item, index) {
+                    this.resourceMonitor.appendMessage({ who: "there.are", what: item.who });
+                }));
+            }));
+
+            this.resourceMonitor.socket.on("someone.said", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "someone.said", what: data.what + " by " + data.who });
+
+                if (typeof data.what.toDo != "undefined" && data.what.toDo != null) {
+                    this.resourceMonitor.whatToDo(data);
+                }
+            }));
+
+            this.resourceMonitor.socket.on("someone.joined", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "someone.joined", what: data.who });
+            }));
+
+            this.resourceMonitor.socket.on("someone.left", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "someone.left", what: data.who });
+            }));
+
+            this.resourceMonitor.socket.on("someone.beat", lang.hitch(this, function (data) {
+                this.resourceMonitor.appendMessage({ who: "someone.beat", what: data.when + " by " + data.who });
+            }));
+        }
+    });
+});
---
+++ js/app/util/ResourceMonitor.js
@@ -0,0 +1,153 @@
+﻿define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/node!repl",
+    "dojo/node!util",
+    "dojo/node!socket.io-client",
+    "dojo/node!edge",
+    "app/util/ResourceConnectionHelper",
+    "app/util/ResourceInboundHelper",
+    "app/util/ResourceOutboundHelper"
+], function (declare, lang, repl, util, io, edge, ResourceConnectionHelper, ResourceInboundHelper, ResourceOutboundHelper) {
+    return declare("app.util.ResourceMonitor", null, {
+        resourceUrl: null,
+        who: "anonymous",
+        socket: null,
+        resourceConnectionHelper: null,
+        resourceInboundHelper: null,
+        resourceOutboundHelper: null,
+        replContext: null,
+        constructor: function (kwArgs) {
+            lang.mixin(this, kwArgs);
+        },
+        appendMessage: function (data) {
+            console.log("%s: %s", data.who, data.what);
+        },
+        logMessage: function (result) {
+            if (result.status) {
+                this.appendMessage({ who: "System (Succeeded)", what: result.message });
+            }
+            else {
+                this.appendMessage({ who: "System (Failed)", what: result.message });
+            }
+        },
+        parseUrl: function (url) {
+            var urlRegExp = new RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?$", "i");
+            var urlArray = urlRegExp.exec(url);
+
+            if (urlArray[4] != null) {
+                var authorityRegExp = new RegExp("^(([^@]+)@)?([^:]*)(:(.*))?$", "i");
+                var authorityArray = authorityRegExp.exec(urlArray[4]);
+            }
+
+            var parseUrl = {
+                "schemeName": urlArray[2],
+                "userInfo": (urlArray[4] == null ? null : authorityArray[2]),
+                "hostName": (urlArray[4] == null ? null : authorityArray[3]),
+                "port": (urlArray[4] == null ? null : authorityArray[5]),
+                "path": urlArray[5],
+                "query": urlArray[7],
+                "fragment": urlArray[9]
+            };
+
+            return parseUrl;
+        },
+        setResourceUrl: function (data) {
+            this.resourceUrl = data.url;
+            var parsedUrl = this.parseUrl(this.resourceUrl);
+            this.socket = io.connect(parsedUrl.schemeName + "://" + parsedUrl.hostName + ":" + (typeof parsedUrl.port == "undefined" ? "80" : parsedUrl.port), { "force new connection": false });
+            this.replContext.socket = this.socket;
+
+            this.resourceConnectionHelper.onMessage();
+        },
+        iAm: function (data) {
+            this.resourceOutboundHelper.handleIAm(data);
+        },
+        iAmNoMore: function (data) {
+            this.resourceOutboundHelper.handleIAmNoMore(data);
+        },
+        heartbeat: function () {
+            this.resourceOutboundHelper.handleHeartbeat();
+        },
+        tellOther: function (data) {
+            this.resourceOutboundHelper.handleTellOther(data);
+        },
+        tellSomeone: function (data) {
+            this.resourceOutboundHelper.handleTellSomeone(data);
+        },
+        whoAreThere: function () {
+            this.resourceOutboundHelper.handleWhoAreThere();
+        },
+        whatToDo: function (data) {
+            switch (data.what.toDo) {
+                case "updateYourDetails":
+                    edge.func("cs", {
+                        source: "C:\\Projects\\MyApp11\\cs\\GetMyDetails.cs",
+                        references: ["System.Management.dll"]
+                    })({
+                        data: {
+                            who: this.who
+                        }
+                    }, lang.hitch(this, function (error, result) {
+                        if (error) {
+                            this.appendMessage({ who: "edge", what: util.inspect(error, { showHidden: false, depth: 2 }) });
+                        }
+                        else {
+                            this.appendMessage({ who: "edge", what: util.inspect(result, { showHidden: false, depth: 5 }) });
+
+                            this.tellSomeone({
+                                whom: data.who,
+                                what: {
+                                    toDo: "updateHisDetails",
+                                    details: result.data
+                                }
+                            });
+                        }
+                    }));
+
+                    break;
+            }
+        },
+        postCreate: function () {
+            this.inherited(arguments);
+
+            this.resourceConnectionHelper = new ResourceConnectionHelper({
+                resourceMonitor: this
+            });
+
+            this.resourceInboundHelper = new ResourceInboundHelper({
+                resourceMonitor: this
+            });
+
+            this.resourceOutboundHelper = new ResourceOutboundHelper({
+                resourceMonitor: this
+            });
+
+            this.replContext = repl.start({
+                prompt: "",
+                ignoreUndefined: true,
+                writer: function (data) {
+                    return "OK";
+                }
+            }).context;
+
+            this.replContext.logMessage = lang.hitch(this, this.logMessage);
+
+            this.replContext.iAm = lang.hitch(this, this.iAm);
+            this.replContext.iAmNoMore = lang.hitch(this, this.iAmNoMore);
+            this.replContext.heartbeat = lang.hitch(this, this.heartbeat);
+            this.replContext.tellOther = lang.hitch(this, this.tellOther);
+            this.replContext.tellSomeone = lang.hitch(this, this.tellSomeone);
+            this.replContext.whoAreThere = lang.hitch(this, this.whoAreThere);
+        },
+        destroy: function () {
+            this.inherited(arguments);
+
+            if (this.who != "anonymous") {
+                this.iAmNoMore({
+                    whoAmI: this.who
+                });
+            }
+        }
+    });
+});
